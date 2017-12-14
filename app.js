3d5c26847b907c40ce38ca00de5a4d38251da5be
Jeremiah Judkins <jeremiahjudkins@Jeremiah-Judkinss-iMac.local>
2017-12-14 14:21:01 -0800

    To-Do-assignment8-delete-callback

--- app.js
+++ app.js
@@ -1,54 +1,82 @@
+//build function that will control everything
 function onReady() {
+  let id = 0;
+  //Creates and houses the current state of to-do list
   let toDos = [];
+  //This accesses the HTML form element
+  const addToDoForm = document.getElementById('addToDoForm');
+  //Build function that creates/adds list items.
   function createNewToDo() {
-    if (!newToDoText.value) {return; }
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
+    //This accesses the HTML input element
+    const newToDoText = document.getElementById('newToDoText');
+    //Avoid submission of empty To-Do items. Prevent this by executing a return
+    //if the value for newToDoText is falsy.
+    if (!newToDoText.value) {
+      return;
+    }
+    //Adds new item to the toDos array
+    toDos.push ({
+      title: newToDoText.value,
+      complete: false,
+      id : id
+    });
+    id++;
+    //Clears the text in the form input field so user doesn't need to
+    //Initialize before the forEach function.
+    newToDoText.value = '';
+    renderTheUI();
+  }
+
+  //callback function that compares the id of each item in the to-do list with the id parameter
+  function destroyDelete (id) {
+    toDos = toDos.filter(item => item.id !== id); //<-- Each Todo item in the todo list !== ID reference parameter.
+
+
+    renderTheUI();
+  }
+  //Visual element of To-Do list
+  function renderTheUI (){
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+    //Parts of the to do item
+    toDos.forEach(function(toDo){
+      //Create a new Li
+      const newLi = document.createElement('li')
+      //Create a new input
+      const checkbox = document.createElement('input');
+      //Create a new deleteButton
+      const deleteButton =  document.createElement('button');
+      //Set the inputs type to checkbox
+      checkbox.type = 'checkbox';
+
+      deleteButton.type = 'button';
+      //Labels delete button with text.
+      deleteButton.textContent = 'Delete';
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      //Append the deleteButton to each newLi
+      newLi.appendChild(deleteButton);
+    //deleteButton Uses specific ID to find and remove the To-Do from the array.
+    deleteButton.addEventListener('click', event => {
+     destroyDelete(toDo.id);//<---deleteButton specific ID input.
+    });
+
+    });
+  }
+  //Event Listener - catches 'submit', prevents page reload,
+  // and invokes the function createNewToDo
+//Use the submit event of the form element.
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
-  newToDoText.value = '';
-
-  renderTheUI(toDos);
+  renderTheUI();
 }
-
-function renderTheUI() {
-  const toDoList = document.getElementById('toDoList');
-  toDoList.textContent = '';
-  toDos.forEach(function(toDo) {
-
-    const newLi = document.createElement('li');
-
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-
-    const title = document.createElement('span');
-
-    newLi.textContent = toDo.title;
-
-    toDoList.appendChild(newLi);
-    newLi.appendChild(checkbox);
-  });
-}
-
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
-
-});
-
- renderTheUI(toDos);
-}
-//The HTML form, so that we can create a new to-do when the user submits the form.
-  const addToDoForm = document.getElementById('addToDoForm')
-//The text input, so that we can get the text for the title of each to-do.
-  const newToDoText = document.getElementById('newToDoText')
-//The todo list itself so we can re-render it when we add or remove todo items.
-const toDoList = document.getElementById('toDoList')
-
-
-
 
 window.onload = function() {
-onReady();
-
+  onReady();
 };
